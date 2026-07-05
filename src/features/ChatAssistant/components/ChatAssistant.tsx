ac1000bc34a1b2f07961d01efac74442335f2414
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2026-07-05 19:14:00 -0300

    Add ChatAssistant component to integrate ChatButton and ChatPanel for enhanced chat functionality.

---
+++ src/features/ChatAssistant/components/ChatAssistant.tsx
@@ -0,0 +1,11 @@
+import ChatButton from "./ChatButton";
+import ChatPanel from "./ChatPanel";
+
+export default function ChatAssistant() {
+	return (
+		<>
+			<ChatButton />
+			<ChatPanel />
+		</>
+	);
+}
